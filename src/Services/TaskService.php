515f84bf3278c5f45ae4f3d073760fbd76d5e29a
Gabriel Aires <gvaires@gmail.com>
2025-03-16 15:57:02 -0300

    feat: create TaskService completed

--- src/Services/TaskService.php
+++ src/Services/TaskService.php
@@ -1,4 +1,7 @@
 <?php
+//respeitando e seguindo o PSR-4 para a estrutura de diretórios e namespaces do PHP e o PSR-12 para a formatação do código
+//https://www.php-fig.org/psr/psr-4/
+//https://www.php-fig.org/psr/psr-12/
 
 namespace TaskManager\Services;
 
@@ -6,5 +9,20 @@ use TaskManager\Models\Task;
 
 class TaskService
 {
-    
+    private array $tasks = [];
+
+    public function addTask(Task $task): void
+    {
+        $this->tasks[$task->getId()] = $task;
+    }
+
+    public function getTasks(): array
+    {
+        return $this->tasks;
+    }
+
+    public function getTaskId(string $id): ?Task
+    {
+        return isset($this->tasks[$id]) ?? null;
+    }
 }
