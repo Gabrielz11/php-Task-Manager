be288a20d29dded454f2ba49aa3fc9cd5d05c359
Gabriel Aires <gvaires@gmail.com>
2025-03-16 16:23:47 -0300

    feat: create index and simulation request post or get for test

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use TaskManager\Controllers\TaskController;
+use TaskManager\Services\TaskService;
+use Laminas\Diactoros\ServerRequestFactory;
+
+$request = ServerRequestFactory::fromGlobals();
+
+$taskService = new TaskService();
+
+$controller = new TaskController($taskService);
+
+/**
+ * Simulação de Request - POST - Aqui estamos criando uma nova tarefa e retornando um JSON com a mensagem de sucesso
+ */
+if($request->getMethod() == 'POST') {
+    $response = $controller->createTask($request);
+} else {
+    $response = $controller->getTasks();
+}
+
+http_response_code($response->getStatusCode());
+echo $response->getBody();
